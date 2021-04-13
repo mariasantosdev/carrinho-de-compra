98204684bd5740480f4899d06983058d08cfe531
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-04-13 12:52:24 -0300

    Arrumando as classes model

--- src/main/java/br/com/improving/carrinho/Item.java
+++ src/main/java/br/com/improving/carrinho/Item.java
@@ -11,70 +11,33 @@ public class Item {
     private BigDecimal valorUnitario;
     private int quantidade;
 
-    /**
-     * Construtor da classe Item.
-     * \
-     * @param produto
-     * @param valorUnitario
-     * @param quantidade
-     */
     public Item(Produto produto, BigDecimal valorUnitario, int quantidade) {
+        this.produto = produto;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
     }
 
-    /**
-     * Retorna o produto.
-     *
-     * @return Produto
-     */
     public Produto getProduto() {
+        return produto;
     }
 
-    /**
-     * Seta o produto.
-     *
-     * @param produto
-     */
     public void setProduto(Produto produto) {
+        this.produto = produto;
     }
 
-    /**
-     * Retorna o valor unitário do item.
-     *
-     * @return BigDecimal
-     */
     public BigDecimal getValorUnitario() {
+        return valorUnitario;
     }
 
-    /**
-     * Seta o valor unitário do item.
-     *
-     * @param valorUnitario
-     */
     public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
     }
 
-    /**
-     * Retorna a quantidade dos item.
-     *
-     * @return int
-     */
     public int getQuantidade() {
+        return quantidade;
     }
 
-    /**
-     * Seta a quantidade do item.
-     *
-     * @param quantidade
-     */
     public void setQuantidade(int quantidade) {
-    }
-
-    /**
-     * Retorna o valor total do item.
-     *
-     * @return BigDecimal
-     */
-    public BigDecimal getValorTotal() {
-
+        this.quantidade = quantidade;
     }
 }
--- src/main/java/br/com/improving/carrinho/Produto.java
+++ src/main/java/br/com/improving/carrinho/Produto.java
@@ -8,48 +8,27 @@ package br.com.improving.carrinho;
  * mesmo código.
  */
 public class Produto {
-
     private Long codigo;
     private String descricao;
 
-    /**
-     * Construtor da classe Produto.
-     *
-     * @param codigo
-     * @param descricao
-     */
     public Produto(Long codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
     }
 
-    /**
-     * Retorna o código da produto.
-     *
-     * @return Long
-     */
     public Long getCodigo() {
+        return codigo;
     }
 
-    /**
-     * Seta o código do produto.
-     *
-     * @param codigo
-     */
     public void setCodigo(Long codigo) {
+        this.codigo = codigo;
     }
 
-    /**
-     * Retorna a descrição do produto.
-     *
-     * @return String
-     */
     public String getDescricao() {
+        return descricao;
     }
 
-    /**
-     * Seta a descrição do produto.
-     *
-     * @param descricao
-     */
     public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
